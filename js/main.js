d259c9f375b9aac61b4057206e4a8211af020456
Quick00 <dammusicgames@gmail.com>
2019-03-06 15:12:53 +0100

    Bestanden

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+open = false;
+function MenuOpen(){
+
+        document.getElementById("invisible").style.height = "200px";
+        document.getElementById("openbtn").style.opacity = 0;
+
+
+
+
+}
+
+function CloseBtn() {
+
+    document.getElementById("invisible").style.height = "0px";
+    document.getElementById("openbtn").style.opacity = 1;
+}
