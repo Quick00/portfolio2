7c6cd9212cd28e17027aeee9461648f105187879
Quick00 <dammusicgames@gmail.com>
2019-03-07 12:16:42 +0100

    Mobile testing

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ function MenuOpen(){
 
         document.getElementById("invisible").style.height = "175px";
         document.getElementById("openbtn").style.opacity = 0;
+        document.getElementById("closebtn").style.opacity = 1;
 
 
 
@@ -14,4 +15,5 @@ function CloseBtn() {
 
     document.getElementById("invisible").style.height = "0px";
     document.getElementById("openbtn").style.opacity = 1;
+    document.getElementById("closebtn").style.opacity = 0;
 }
